e51cf9bc4abf3430a2a7af981d33a4421a75834d
Gleefre <varedif.a.s@gmail.com>
2022-11-03 10:44:21 +0100

    level 2

--- levels.lisp
+++ levels.lisp
@@ -4,6 +4,12 @@
   (with-map '((0 0))
     (deftrip '(1/2 0) '(-1/2 1/2) :red)))
 
+(defun level-2 ()
+  (with-map '((0 0) (1 0))
+    (deftrip '(1/2 0) '(1/2 1/2) :red)
+    (deftrip '(-2/3 1/3) '(-2/3 1/3) :yellow)))
+
 (defun level (n)
   (case n
-    (1 (level-1))))
+    (1 (level-1))
+    (2 (level-2))))
