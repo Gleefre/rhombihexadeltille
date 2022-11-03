878f40982864d5aa9de63e071218e61e5b008297
Gleefre <varedif.a.s@gmail.com>
2022-11-03 11:54:10 +0100

    added sketch dependency, function to draw maps/nodes, dummy window

--- packages.lisp
+++ packages.lisp
@@ -1,14 +1,9 @@
 (defpackage #:rhombihexadeltille/core
   (:use #:cl)
-  (:export #:node
+  (:export #:node #:node-inside #:node-outside
            #:make-level-map
-           #:rotate
-           #:key-rotate
-           #:with-map
-           #:deftrip
-           #:defrotate
-           #:deftrash
-           #:defbin
+           #:rotate #:key-rotate
+           #:with-map #:deftrip #:defrotate #:deftrash #:defbin
            #:win?)
   (:nicknames #:rht/core))
 
@@ -16,3 +11,11 @@
   (:use #:cl #:rht/core)
   (:export #:level)
   (:nicknames #:rht/levels))
+
+(defpackage #:rhombihexadeltille/draw
+  (:use #:cl #:rht/core #:rht/levels)
+  (:nicknames #:rht/draw)
+  (:import-from #:sketch
+                #:defsketch #:translate #:with-pen #:make-pen
+                #:ngon #:circle
+                #:+red+ #:+green+ #:+yellow+ #:+blue+ #:+black+))
--- rhombihexadeltille.asd
+++ rhombihexadeltille.asd
@@ -2,6 +2,8 @@
   :description "A little puzzle game"
   :version "0.0.0"
   :author "Gleefre <varedif.a.s@gmail.com>"
+  :depends-on ("sketch")
   :components ((:file "packages")
                (:file "core")
-               (:file "levels")))
+               (:file "levels")
+               (:file "draw")))
